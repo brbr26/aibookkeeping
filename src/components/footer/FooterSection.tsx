9167a1f5b3e009ec5bd24137e2c86e23cd95b230
brbr26 <47866216+brbr26@users.noreply.github.com>
2025-01-16 09:21:22 -0500

    Update FooterSection.tsx

--- src/components/footer/FooterSection.tsx
+++ src/components/footer/FooterSection.tsx
@@ -6,7 +6,12 @@ interface FooterSectionProps {
   className?: string;
 }
 
-export const FooterSection: FC<FooterSectionProps> = ({ title, children, className = "md:col-span-2" }) => (
+export const FooterSection: FC<FooterSectionProps> = ({
+  title,
+  children,
+- className = "md:col-span-2"
++ className = ""
+}) => (
   <div className={className}>
     <h3 className="font-semibold text-lg mb-4 md:mb-0">{title}</h3>
     {children}
