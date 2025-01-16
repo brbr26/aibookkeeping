cafaaefb10ebd8b3cc632543e97b5dccf3a957c1
brbr26 <47866216+brbr26@users.noreply.github.com>
2025-01-16 14:43:12 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Rename these imports to something clearer if you want
 import Index from "./pages/Index";
 import Services from "./pages/Services";
 import Login from "./pages/Login";
@@ -10,10 +11,15 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Home page */}
         <Route path="/" element={<Index />} />
+
+        {/* Other pages */}
         <Route path="/services" element={<Services />} />
         <Route path="/login" element={<Login />} />
         <Route path="/industries" element={<Industries />} />
+
+        {/* Make sure these match your IndustryCard "to" or "href" props */}
         <Route path="/ecommerce" element={<Ecommerce />} />
         <Route path="/law-firms" element={<LawFirms />} />
       </Routes>
